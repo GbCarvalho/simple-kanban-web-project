acfad53805de14bd0abd580be5cb856162cf6f46
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2020-10-19 01:19:10 -0300

    Initial imports and api configuration

---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,26 @@
+import { Icon } from '@iconify/react';
+import addTaskButton from '@iconify/icons-ic/round-add-task';
+import cancelTaskButton from '@iconify/icons-ic/round-close';
+import nextTaskButton from '@iconify/icons-ic/round-navigate-next';
+import backTaskButton from '@iconify/icons-ic/round-navigate-before';
+import check from '@iconify/icons-ic/twotone-check';
+import add from '@iconify/icons-ic/round-plus';
+import React, { FormEvent, useEffect, useState } from 'react';
+
+import {
+  Container,
+  CardContainer,
+  CardHeader,
+  Page,
+  Card,
+  AddCard,
+  ButtonsContainer,
+  CardForm
+} from './styles';
+import api from '../../utils/api';
+
+
+function MainPage() {
+}
+
+export default MainPage;
